64e8bc23527fe6f7464d616b90bad6e9e02f22a3
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-09-21 12:11:07 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,23 @@
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { ConnectWallet } from '@thirdweb-dev/react';
 
 export default function Navbar() {
-     return(
-        <div className={styles.navbar}>
-         <Link href="/">
-            <p className={styles.gradient1} 
-            style={{
-                cursor: 'pointer', 
-                fontSize: '1.2 rem',
-                fontWeight:'bold'
-                }}>
-                    Tagen's Portfolio
-                    </p>
-         </Link>
-        </div>
-     )
-};
+  return (
+    <div className={styles.navbar}>
+      <Link href="/">
+        <p
+          className={styles.gradient1}
+          style={{
+            cursor: 'pointer',
+            fontSize: '1.2rem', // Remove the space here
+            fontWeight: 'bold', // Remove the extra space here
+          }}
+        >
+          Tagen's Portfolio
+        </p>
+      </Link>
+      <ConnectWallet />
+    </div>
+  );
+}
