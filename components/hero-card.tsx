b90f2b80e5217c85ce808d345dc5f6a1a0ed0813
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-09-23 12:26:35 +0100

    update

--- components/hero-card.tsx
+++ components/hero-card.tsx
@@ -4,34 +4,35 @@ import styles from '../styles/Home.module.css';
 type HeroCardProps = {
     isLoading: boolean;
     title: string;
-    description:string;
+    description: string;
     image: string;
 };
 
 export default function HeroCard(props: HeroCardProps) {
-
     return (
-      <>
-         { props.isLoading ? (
-            <div className={styles.loadingText}>
-               <p>Loading...</p>
-               </div>
-             ) : (
-                <div className={styles.heroCardContainer}>
-                   <MediaRenderer
-                      src={props.image}
-                      width="100%"
-                      height="auto"
-                      className={styles.heroCardContractImage} 
-                   />
-                   <div className={styles.heroCardContent}>
-                       <h1 className={styles.gradientText1}>
-                        {props.title}
-                        </h1>
-                        <p>{props.description}</p>
-                   </div>
+        <>
+            {props.isLoading ? (
+                <div className={styles.loadingText}>
+                    <p>Loading...</p>
                 </div>
-             )}
-      </>
-    )
+            ) : (
+                <div className={styles.heroCardContainer}>
+                    <MediaRenderer
+                        src={props.image}
+                        width="100%"
+                        height="auto"
+                        className={styles.heroCardContractImage}
+                    />
+                    <div className={styles.heroCardContent}>
+                        <h1 className={styles.gradientText1}>
+                            {props.title}
+                        </h1>
+                        <p className={styles.description}>
+                            {props.description}
+                        </p>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
